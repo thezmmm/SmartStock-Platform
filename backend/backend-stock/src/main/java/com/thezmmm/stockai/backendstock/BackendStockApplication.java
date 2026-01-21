91d89a85f3042cb74eeb546ccd614382729edadb
thezmmm <1563081082@qq.com>
2026-01-21 15:38:24 +0200

    feat: initialize backend-stock module

---
+++ backend/backend-stock/src/main/java/com/thezmmm/stockai/backendstock/BackendStockApplication.java
@@ -0,0 +1,13 @@
+package com.thezmmm.stockai.backendstock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendStockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendStockApplication.class, args);
+    }
+
+}
